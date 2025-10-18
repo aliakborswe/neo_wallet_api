03609cdc177a9cf6b69eb48d7f795f0e64eff1d6
aliakborswe <aliakbor.js@gmail.com>
2025-10-18 17:05:35 +0600

    server deployment successful

--- src/app/utils/setCookie.ts
+++ src/app/utils/setCookie.ts
@@ -1,23 +1,26 @@
 import { Response } from "express";
+import { envVars } from "../config/env";
 
 export interface AuthTokens {
   accessToken?: string;
   refreshToken?: string;
 }
 
+const isProduction = envVars.NODE_ENV === "production";
+
 export const setAuthCookie = (res: Response, tokenInfo: AuthTokens) => {
   if (tokenInfo.accessToken) {
     res.cookie("accessToken", tokenInfo.accessToken, {
       httpOnly: true,
-      secure: false,
-      // sameSite: "none",
+      secure: isProduction,
+      sameSite: isProduction ? "none" : "lax",
     });
   }
   if (tokenInfo.refreshToken) {
     res.cookie("refreshToken", tokenInfo.refreshToken, {
       httpOnly: true,
-      secure: false,
-      // sameSite: "none",
+      secure: isProduction,
+      sameSite: isProduction ? "none" : "lax",
     });
   }
 };
